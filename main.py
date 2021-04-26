c71e45be92f9d05ea6bf6ba070f313857aae674c
min1233 <minsug937@naver.com>
2021-04-26 17:04:29 +0900

    version 0.001

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
-url = "http://gvka2m4qt5fod2fltkjmdk4gxh5oxemhpgmnmtjptms6fkgfzdd62tad.onion"
+search_url = "https://onionsearchengine.com/search.php?search=TEST&submit=Search&page="
+
+http_list = []
+
+def get_http(a_tag):
+    global http_list
+    http_re = "http[s]?:\/\/[a-zA-Z0-9\.]*\.onion\/"
+    for tag in a_tag:
+        tmp = re.findall(http_re,str(tag))
+        if(tmp!=[]):
+            http_list.append(tmp[0])
+    return list(set(http_list))
+
 session = requests.session()
 
 session.proxies = {
@@ -8,6 +22,16 @@ session.proxies = {
         "https":"socks5h://127.0.0.1:9050",
 }
 
-rep = session.get(url)
+if __name__ == "__main__":
+    for i in range(1,20):
+        rep = requests.get(search_url.replace("TEST","gun")+str(i))
+        if(rep.text.find("Sorry, there are no matching result fo")!=-1):
+            print("Search END")
+            break
+        else:
+            bs = BeautifulSoup(rep.text, 'html.parser')
+            a_tag = bs.select('a')
+            http_list = get_http(a_tag)
 
-print(rep.text)
+    for http in http_list:
+        print(http)
