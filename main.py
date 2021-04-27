361a01d04686e8b6b9b21c185ed37d3a5341423c
min1233 <minsug937@naver.com>
2021-04-27 14:52:42 +0900

    version 0.0.2

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
+import mysqllib as mysql_class
 import requests
-from bs4 import BeautifulSoup
 import re
+from bs4 import BeautifulSoup
 
 search_url = "https://onionsearchengine.com/search.php?search=TEST&submit=Search&page="
 
 http_list = []
-
-def get_http(a_tag):
-    global http_list
-    http_re = "http[s]?:\/\/[a-zA-Z0-9\.]*\.onion\/"
-    for tag in a_tag:
-        tmp = re.findall(http_re,str(tag))
-        if(tmp!=[]):
-            http_list.append(tmp[0])
-    return list(set(http_list))
 
 session = requests.session()
 
@@ -22,16 +14,43 @@ session.proxies = {
         "https":"socks5h://127.0.0.1:9050",
 }
 
-if __name__ == "__main__":
-    for i in range(1,20):
-        rep = requests.get(search_url.replace("TEST","gun")+str(i))
+def get_http_link(a_tag):
+    global http_list
+    http_re = "http[s]?:\/\/[a-zA-Z0-9\.]*\.onion\/"
+    for tag in a_tag:
+        tmp = re.findall(http_re,str(tag))
+        if(tmp!=[]):
+            http_list.append(tmp[0])
+    http_list = list(set(http_list))
+
+def search_keyword(keyword, i=100):
+    for i in range(1, i):
+        rep = requests.get(search_url.replace("TEST",keyword)+str(i))
         if(rep.text.find("Sorry, there are no matching result fo")!=-1):
             print("Search END")
             break
         else:
             bs = BeautifulSoup(rep.text, 'html.parser')
             a_tag = bs.select('a')
-            http_list = get_http(a_tag)
+            get_http_link(a_tag)
 
-    for http in http_list:
-        print(http)
+if __name__ == "__main__":
+    search_keyword("gun", 3)
+    for i, url in enumerate(http_list):
+        try:
+            rep = session.get(url)
+            bs = BeautifulSoup(rep.text, 'html.parser')
+            
+            title = bs.select_one('title').text
+            file_path = "./data/"+str(i)+".html"
+            
+            mysql = mysql_class.mysql()
+            mysql.insert_data(url, title, file_path)
+
+            f = open(file_path,"w")
+            f.write(rep.text)
+            f.close()
+            
+        except Exception as e:
+            print(e)
+            continue
