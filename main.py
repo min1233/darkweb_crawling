0d935c91cc4ebecd510a348ae9b00740e6a1b439
min1233 <minsug937@naver.com>
2021-04-28 17:40:03 +0900

    Version 0.0.3

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import mysqllib as mysql_class
 import requests
 import re
+import os
+import threading
 from datetime import datetime
 from bs4 import BeautifulSoup
-import os
 
 search_url = "https://onionsearchengine.com/search.php?search=TEST&submit=Search&page="
-
-http_list = []
-
+# onion Search Engine
+#search_url2 = "http://visitorfi5kl7q7i.onion/search/?q=TEST&page="
 session = requests.session()
+
+# color
+bold = "\033[1m"
+end = "\033[0m"
 
 session.proxies = {
         "http":"socks5h://127.0.0.1:9050",
         "https":"socks5h://127.0.0.1:9050",
 }
 
-def get_http_link(a_tag):
-    global http_list
+# find onion Link
+def get_http_link(a_tag, http_list):
     http_re = "http[s]?:\/\/[a-zA-Z0-9\.]*\.onion\/"
     for tag in a_tag:
         tmp = re.findall(http_re,str(tag))
         if(tmp!=[]):
             http_list.append(tmp[0])
-    http_list = list(set(http_list))
+    return list(set(http_list))
 
-def search_keyword(keyword, i=100):
-    for i in range(1, i):
+# search for keyword at onionsearchengine.com
+def search_keyword(keyword, index=100):
+    http_list = []
+    for i in range(1, index):
         rep = requests.get(search_url.replace("TEST",keyword)+str(i))
         if(rep.text.find("Sorry, there are no matching result fo")!=-1):
             print("Search END")
@@ -34,19 +40,23 @@ def search_keyword(keyword, i=100):
         else:
             bs = BeautifulSoup(rep.text, 'html.parser')
             a_tag = bs.select('a')
-            get_http_link(a_tag)
+            http_list = get_http_link(a_tag, http_list)
+    print(f"Keyword : {keyword}, Found",len(http_list),"onion")
+    return http_list
 
-if __name__ == "__main__":
-    dir_path = "./data/"+datetime.today().strftime("%Y_%m_%d")
-    os.system(f"mkdir {dir_path}")
-    search_keyword("gun", 100)
-    for i, url in enumerate(http_list):
+# access onion url in http list
+def find_onion(keyword, http_list):
+    count = 0;
+    dir_path = "./data/"+datetime.today().strftime("%Y_%m_%d")+"/"+keyword
+    os.system(f"mkdir -p {dir_path}")
+    for url in http_list:
         try:
             rep = session.get(url)
             bs = BeautifulSoup(rep.text, 'html.parser')
             
             title = bs.select_one('title').text
-            file_path = dir_path+"/"+str(i)+".html"
+            file_path = dir_path+"/"+str(count)+".html"
+            print(f"{bold}[{keyword}]{end} Find Title : {title}")
             
             mysql = mysql_class.mysql()
             mysql.insert_data(url, title, file_path)
@@ -54,6 +64,28 @@ if __name__ == "__main__":
             f = open(file_path,"w")
             f.write(rep.text)
             f.close()            
+
+            # get onion url in html code
+            bs = BeautifulSoup(rep.text, 'html.parser')
+            a_tag = bs.select('a')
+            tmp_list = get_http_link(a_tag, [])
+            
+            # Find new onion url without duplication
+            for tmp in tmp_list:
+                if tmp not in http_list:
+                    http_list.append(tmp)
+            count += 1;
         except Exception as e:
-            print(e)
+#            print(e);
             continue
+    mysql.close()
+
+# main thread function
+def main_fun(keyword, index):
+    http_list = search_keyword(keyword, index)
+    find_onion(keyword, http_list)
+
+if __name__ == "__main__":
+    main_fun("gun", 100);
+#    t = threading.Thread(tarrget=keyword_search, args=["gun",100])
+#    t.start()
